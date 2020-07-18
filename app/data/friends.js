fbc61fa37b26e7a4187466ae5cdb251e185e957f
Sandy Smith <toosandra@gmail.com>
2020-07-17 21:46:04 -0400

    Debugging.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,117 +4,115 @@
 var friends = [
 
   {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
     ]
   },
   {
-    name: "Brennan Elliott",
-    photo: "https://www.hallmarkchannel.com/all-of-my-heart-the-wedding/cast/brennan-elliott",
-    scores: [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+    "name": "Brennan Elliott",
+    "photo": "https://www.hallmarkchannel.com/all-of-my-heart-the-wedding/cast/brennan-elliott",
+    "scores": [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
     ]
   },
   {
-    name: "Andrew Walker",
-    photo: "https://www.hallmarkchannel.com/cast/andrew-walker",
-    scores: [
-      4,
-      2,
-      5,
-      1,
-      3,
-      2,
-      2,
-      1,
-      3,
-      2
+    "name": "Andrew Walker",
+    "photo": "https://www.hallmarkchannel.com/cast/andrew-walker",
+    "scores": [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
     ]
   },
   {
-    name: "Danica McKeller",
-    photo: "https://www.hallmarkchannel.com/perfect-match/cast/danica-mckellar",
-    scores: [
-      4,
-      5,
-      2,
-      4,
-      2,
-      4,
-      3,
-      3,
-      2,
-      3,
-      3
+    "name": "Danica McKeller",
+    "photo": "https://www.hallmarkchannel.com/perfect-match/cast/danica-mckellar",
+    "scores": [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
     ]
   },
   {
-    name: "Ryan Paevey",
-    photo: "https://www.pinterest.com/pin/225461525075208919/",
-    scores: [
-      5,
-      5,
-      2,
-      2,
-      2,
-      4,
-      1,
-      3,
-      2,
-      5,
-      5
+    "name": "Ryan Paevey",
+    "photo": "https://www.pinterest.com/pin/225461525075208919/",
+    "scores": [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
     ]
   },
   {
-    name: "Lacey Chabert",
-    photo: "https://www.hallmarkchannel.com/all-of-my-heart/cast/lacey-chabert",
-    scores: [
-      3,
-      3,
-      4,
-      2,
-      2,
-      1,
-      3,
-      2,
-      2,
-      3
+    "name": "Lacey Chabert",
+    "photo": "https://www.hallmarkchannel.com/all-of-my-heart/cast/lacey-chabert",
+    "scores": [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
     ]
   },
   {
-    name: "Jill Wagner",
-    photo: "https://www.hallmarkchannel.com/autumn-dreams/cast/jill-wagner",
-    scores: [
-      4,
-      4,
-      2,
-      3,
-      2,
-      2,
-      3,
-      2,
-      4,
-      5
+    "name": "Jill Wagner",
+    "photo": "https://www.hallmarkchannel.com/autumn-dreams/cast/jill-wagner",
+    "scores": [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
   }
 ];
--- server.js
+++ server.js
@@ -14,11 +14,12 @@ var fs = require("fs");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 7900;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ================================================================================
 // ROUTER
